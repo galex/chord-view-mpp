1ae0e48b11e6c63b9202035c1fd46957f193d1d0
Alexandre Gherschon <alexandre.gherschon@gmail.com>
2018-06-25 08:25:42 +0300

    Try the Kotlin/Native thing

---
+++ iOSApp/KotlinNativeFramework/src/main/kotlin/KotlinNativeFramework.kt
@@ -0,0 +1,13 @@
+//
+//  KotlinNativeFramework.kt
+//  KotlinNativeFramework
+//
+//  Created by Alexandre Gherschon on 23/06/2018.
+//  Copyright © 2018 galex.co.il. All rights reserved.
+//
+
+import
+
+class KotlinNativeFramework: UIView {
+    fun helloFromKotlin() = "Hello from Kotlin!"
+}
---
+++ iOSApp/iOSApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iOSApp
+//
+//  Created by Alexandre Gherschon on 23/06/2018.
+//  Copyright © 2018 galex.co.il. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
