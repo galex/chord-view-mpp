2c3167fa94199cd5176d84426929808eff989918
Alexandre Gherschon <alexandre.gherschon@gmail.com>
2018-06-27 14:33:39 +0300

    Added Logger implementation to Android

---
+++ AndroidApp/src/main/java/il/co/galex/chordview/common/helper/Logger.kt
@@ -0,0 +1,9 @@
+package il.co.galex.chordview.common.helper
+
+import android.util.Log
+
+actual object Logger {
+    actual fun log(message: String) {
+        Log.d(Logger::class.java.simpleName, message)
+    }
+}
